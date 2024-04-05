6ea154ebff9e36cf2d04a59ab496ecd16cb1fd15
La+021026 <liyida1026@163.com>
2024-04-06 06:14:38 +0800

    test version2

--- src/viewinfo.java
+++ src/viewinfo.java
@@ -117,6 +117,7 @@ public class viewinfo {
     public static void main(String[] args) {
         System.out.println("helloworld");
         System.out.println("final version");
+        System.out.println("final version1");
     }
 }
 
