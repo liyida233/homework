ae49f3ad4ff4abf30fbb3faf23cdd5e4c6adedd9
La+021026 <liyida1026@163.com>
2024-05-07 14:19:24 +0800

    correct version pull test

--- src/viewinfo.java
+++ src/viewinfo.java
@@ -117,7 +117,7 @@ public class viewinfo {
     public static void main(String[] args) {
         System.out.println("helloworld");
         System.out.println("final version");
-        System.out.println("push test");
+        System.out.println("pull test");
     }
 }
 
